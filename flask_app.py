d8c1495761c0cbfa04c6d53c90474e837b76d106
womelnit <womelnit@womelnit-K501LB>
2019-12-19 08:56:02 +0200

    First commit

---
+++ flask_app.py
@@ -0,0 +1,265 @@
+from flask import Flask, render_template
+import mysql.connector
+from flask_socketio import SocketIO
+from flask import request, redirect
+
+
+def first_query(cursor, group, violation):
+   cursor.execute(
+      "select concat(person.surname,' ',person.name,' ',person.patronymic) as full_name from groups join student_group on groups.group_id = student_group.group_id "
+      "join student on student.student_id = student_group.student_id join person on person.person_id = student.person_id"
+      " join violation on person.person_id = violation.person_id join sviolation_kind on violation.violation_kind_id = sviolation_kind.violation_kind_id"
+      " where violation_kind_name = '{0}' and group_code = '{1}'".format(violation, group))
+   student_list = cursor.fetchall()
+   return student_list
+
+
+def second_query(cursor, speciality):
+   cursor.execute(
+      "select group_alias from (select sum(mark_name)/count(student.student_id) as avg, groups.group_code as group_alias from person "
+      "join student on person.person_id = student.person_id join student_group on student.student_id = student_group.student_id "
+      "join groups on groups.group_id = student_group.group_id join speciality on speciality.speciality_id = groups.speciality_id "
+      "join student_marks on student_marks.student_id = student.student_id join smark on smark.mark_id = student_marks.mark_id "
+      "where speciality.speciality_name = '{0}' group by groups.group_id)tt where tt.avg = (select max(avg) "
+      "from (select sum(mark_name)/count(student.student_id) as avg, groups.group_code as group_alias from person "
+      "join student on person.person_id = student.person_id join student_group on student.student_id = student_group.student_id "
+      "join groups on groups.group_id = student_group.group_id join speciality on speciality.speciality_id = groups.speciality_id "
+      "join student_marks on student_marks.student_id = student.student_id join smark on smark.mark_id = student_marks.mark_id "
+      "where speciality.speciality_name = '{0}' group by groups.group_id) t2)".format(speciality))
+   max_avg_point_group = cursor.fetchall()
+   return max_avg_point_group
+
+
+def third_query(cursor, mark_lower_border, speciality):
+   cursor.execute(
+      "select concat(person.surname,' ',person.name,' ',person.patronymic) as full_name from person join student on person.person_id = student.person_id "
+      "join student_group on student.student_id = student_group.student_id join groups on groups.group_id = student_group.group_id "
+      "join speciality on speciality.speciality_id = groups.speciality_id join student_marks on student_marks.student_id = student.student_id "
+      "join smark on smark.mark_id = student_marks.mark_id where (smark.mark_name > {0}) "
+      "and (speciality.speciality_name = '{1}');".format(mark_lower_border, speciality))
+   student_list = cursor.fetchall()
+   return student_list
+
+
+def fourth_query(cursor, punish_kind, contract_kind):
+   cursor.execute(
+      "select concat(person.surname,' ',person.name,' ',person.patronymic) as full_name from person join violation on person.person_id = violation.person_id"
+      " join spunish_kind on spunish_kind.punish_kind_id = violation.punish_kind_id join student on student.person_id = person.person_id"
+      " join contract on contract.student_id = student.student_id join scontract_kind on scontract_kind.contract_kind_id = contract.contract_kind_id"
+      " where spunish_kind.punish_kind_name = '{0}' and scontract_kind.contract_kind_name = '{1}'".format(punish_kind,
+                                                                                                          contract_kind))
+   student_list = cursor.fetchall()
+   return student_list
+
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+socketio = SocketIO(app)
+
+mydatabase = mysql.connector.connect(
+    host = 'localhost', user = 'dbuser',
+    passwd = 'dbuser_1234', database = 'Cist')
+
+
+mycursor = mydatabase.cursor()
+
+#There you can add home page and others. It is completely depends on you
+
+@app.route('/person')
+def person():
+   mycursor.execute('SELECT * FROM person')
+   data = mycursor.fetchall()
+   return render_template('person.html', output_data = data)
+
+@app.route('/violation')
+def violation():
+   mycursor.execute('SELECT * FROM violation')
+   data = mycursor.fetchall()
+   return render_template('violation.html', output_data = data)
+
+@app.route('/sviolation_kind')
+def sviolation_kind():
+   mycursor.execute('SELECT * FROM sviolation_kind')
+   data = mycursor.fetchall()
+   return render_template('sviolation_kind.html', output_data = data)
+
+@app.route('/spunish_kind')
+def spunish_kind():
+   mycursor.execute('SELECT * FROM spunish_kind')
+   data = mycursor.fetchall()
+   return render_template('spunish_kind.html', output_data = data)
+
+@app.route('/student')
+def student():
+   mycursor.execute('SELECT * FROM student')
+   data = mycursor.fetchall()
+   return render_template('student.html', output_data = data)
+
+@app.route('/student_marks')
+def student_marks():
+   mycursor.execute('SELECT * FROM student_marks')
+   data = mycursor.fetchall()
+   return render_template('student_marks.html', output_data = data)
+
+@app.route('/smark')
+def smark():
+   mycursor.execute('SELECT * FROM smark')
+   data = mycursor.fetchall()
+   return render_template('smark.html', output_data = data)
+
+@app.route('/contract')
+def contract():
+   mycursor.execute('SELECT * FROM contract')
+   data = mycursor.fetchall()
+   return render_template('contract.html', output_data = data)
+
+@app.route('/scontract_kind')
+def scontract_kind():
+   mycursor.execute('SELECT * FROM scontract_kind')
+   data = mycursor.fetchall()
+   return render_template('scontract_kind.html', output_data = data)
+
+@app.route('/student_group')
+def student_group():
+   mycursor.execute('SELECT * FROM student_group')
+   data = mycursor.fetchall()
+   return render_template('student_group.html', output_data = data)
+
+@app.route('/groups')
+def groups():
+   mycursor.execute('SELECT * FROM groups')
+   data = mycursor.fetchall()
+   return render_template('groups.html', output_data = data)
+
+@app.route('/speciality')
+def speciality():
+   mycursor.execute('SELECT * FROM speciality')
+   data = mycursor.fetchall()
+   return render_template('speciality.html', output_data = data)
+
+@app.route("/request1", methods=["GET", "POST"])
+def request1():
+
+    answer_list = [["Відповідь буде тут"]]
+    if request.method == "POST":
+
+        req = request.form
+        # print(req)
+        # print(request.form.get("group"))
+        # print(request.form.get("violation"))
+        #print(first_query(mycursor,request.form.get("group"),request.form.get("violation"))[0][0])
+        try:
+         answer_list = first_query(mycursor,request.form.get("group"),request.form.get("violation"))
+        except:
+           pass
+        if len(answer_list)!=0:
+         return render_template("request1.html",output_data = answer_list)
+        else:
+           answer_list = [["Немає таких студентів"]]
+           return render_template("request1.html", output_data=answer_list)
+
+    return render_template("request1.html",output_data = answer_list)
+
+@app.route("/request2", methods=["GET", "POST"])
+def request2():
+
+    answer_list = [["Відповідь буде тут"]]
+    if request.method == "POST":
+
+        req = request.form
+        #print(first_query(mycursor,request.form.get("group"),request.form.get("violation"))[0][0])
+        try:
+         answer_list = second_query(mycursor,request.form.get("speciality"))
+        except:
+           pass
+        if len(answer_list)!=0:
+         return render_template("request2.html",output_data = answer_list)
+        else:
+           answer_list = [["Немає такої спеціальності aбо немає груп"]]
+           return render_template("request2.html", output_data=answer_list)
+
+    return render_template("request2.html",output_data = answer_list)
+
+@app.route("/request3", methods=["GET", "POST"])
+def request3():
+
+    answer_list = [["Відповідь буде тут"]]
+    if request.method == "POST":
+
+        req = request.form
+        # print(req)
+        # print(request.form.get("speciality"))
+        # print(request.form.get("mark"))
+        #print(first_query(mycursor,request.form.get("group"),request.form.get("violation"))[0][0])
+        try:
+         answer_list = third_query(mycursor,request.form.get("mark"),request.form.get("speciality"))
+        except:
+           pass
+        if len(answer_list)!=0:
+         return render_template("request3.html",output_data = answer_list)
+        else:
+           answer_list = [["Немає таких студентів"]]
+           return render_template("request3.html", output_data=answer_list)
+
+    return render_template("request3.html",output_data = answer_list)
+
+
+@app.route("/request4", methods=["GET", "POST"])
+def request4():
+
+    answer_list = [["Відповідь буде тут"]]
+    if request.method == "POST":
+
+        req = request.form
+        # print(req)
+        # print(request.form.get("contract"))
+        # print(request.form.get("punish"))
+        #print(first_query(mycursor,request.form.get("group"),request.form.get("violation"))[0][0])
+        try:
+         answer_list = fourth_query(mycursor,request.form.get("punish"),request.form.get("contract"))
+        except:
+           pass
+        if len(answer_list)!=0:
+         return render_template("request4.html",output_data = answer_list)
+        else:
+           answer_list = [["Немає таких студентів"]]
+           return render_template("request4.html", output_data=answer_list)
+
+    return render_template("request4.html",output_data = answer_list)
+
+
+
+
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
+
+if __name__ == '__main__':
+    socketio.run(app)
+
+# from flask import Flask, render_template
+# from flask_socketio import SocketIO
+#
+# app = Flask(__name__)
+# app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+# socketio = SocketIO(app)
+#
+# @app.route('/')
+# def sessions():
+#     return render_template('session.html')
+#
+# def messageReceived(methods=['GET', 'POST']):
+#     print('message was received!!!')
+#
+# @socketio.on('my event')
+# def handle_my_custom_event(json, methods=['GET', 'POST']):
+#     print('received my event: ' + str(json))
+#     socketio.emit('my response', json, callback=messageReceived)
+#
+# if __name__ == '__main__':
+#     socketio.run(app, debug=True)
---
+++ server.py
@@ -0,0 +1,73 @@
+import mysql.connector
+
+def first_query(cursor, group,violation):
+    cursor.execute("select concat(person.surname,' ',person.name,' ',person.patronymic) as full_name from groups join student_group on groups.group_id = student_group.group_id "
+                   "join student on student.student_id = student_group.student_id join person on person.person_id = student.person_id"
+                   " join violation on person.person_id = violation.person_id join sviolation_kind on violation.violation_kind_id = sviolation_kind.violation_kind_id"
+                   " where violation_kind_name = '{0}' and group_code = '{1}'".format(violation,group))
+    student_list = cursor.fetchall()
+    return student_list
+
+def second_query(cursor, speciality):
+    cursor.execute("select group_alias from (select sum(mark_name)/count(student.student_id) as avg, groups.group_code as group_alias from person "
+                   "join student on person.person_id = student.person_id join student_group on student.student_id = student_group.student_id "
+                   "join groups on groups.group_id = student_group.group_id join speciality on speciality.speciality_id = groups.speciality_id "
+                   "join student_marks on student_marks.student_id = student.student_id join smark on smark.mark_id = student_marks.mark_id "
+                   "where speciality.speciality_name = '{0}' group by groups.group_id)tt where tt.avg = (select max(avg) "
+                   "from (select sum(mark_name)/count(student.student_id) as avg, groups.group_code as group_alias from person "
+                   "join student on person.person_id = student.person_id join student_group on student.student_id = student_group.student_id "
+                   "join groups on groups.group_id = student_group.group_id join speciality on speciality.speciality_id = groups.speciality_id "
+                   "join student_marks on student_marks.student_id = student.student_id join smark on smark.mark_id = student_marks.mark_id "
+                   "where speciality.speciality_name = '{0}' group by groups.group_id) t2)".format(speciality))
+    max_avg_point_group = cursor.fetchall()
+    return max_avg_point_group
+
+def third_query(cursor, mark_lower_border,speciality):
+    cursor.execute("select concat(person.surname,' ',person.name,' ',person.patronymic) as full_name from person join student on person.person_id = student.person_id "
+                   "join student_group on student.student_id = student_group.student_id join groups on groups.group_id = student_group.group_id "
+                   "join speciality on speciality.speciality_id = groups.speciality_id join student_marks on student_marks.student_id = student.student_id "
+                   "join smark on smark.mark_id = student_marks.mark_id where (smark.mark_name > {0}) "
+                   "and (speciality.speciality_name = '{1}');".format(mark_lower_border,speciality))
+    student_list = cursor.fetchall()
+    return student_list
+
+def fourth_query(cursor, punish_kind,contract_kind):
+    cursor.execute("select concat(person.surname,' ',person.name,' ',person.patronymic) as full_name from person join violation on person.person_id = violation.person_id"
+                   " join spunish_kind on spunish_kind.punish_kind_id = violation.punish_kind_id join student on student.person_id = person.person_id"
+                   " join contract on contract.student_id = student.student_id join scontract_kind on scontract_kind.contract_kind_id = contract.contract_kind_id"
+                   " where spunish_kind.punish_kind_name = '{0}' and scontract_kind.contract_kind_name = '{1}'".format(punish_kind,contract_kind))
+    student_list = cursor.fetchall()
+    return student_list
+
+def insert_row_into_table (cursor, table_name, row_of_data):
+    cursor.execute("INSERT INTO {0} VALUES {1}".format(table_name,row_of_data))
+
+def delete_row_from_table (cursor, table_name, primary_key_name, primary_key_id):
+    cursor.execute("DELETE FROM {0} WHERE {1} = '{2}'".format(table_name,primary_key_name,primary_key_id))
+
+def show_all_table (cursor, table_name):
+    cursor.execute("SELECT * FROM {0}".format(table_name))
+    table_list = cursor.fetchall()
+
+    return table_list
+
+def update_row_in_table_by_primary_key(cursor,table_name,row_of_data,prim_key_name,prim_key_value):
+    cursor.execute("UPDATE {0} SET {1} where {2} = {3}".format(table_name,row_of_data,prim_key_name,prim_key_value))
+
+# cist_db = mysql.connector.connect(user='dbuser', password='dbuser_1234',host='localhost',database='Cist')
+# cursor = cist_db.cursor()
+#
+# #update_row_in_table_by_primary_key(cursor,"sviolation_kind", "violation_kind_name = 'Некорректна поведінка'", "violation_kind_id","8")
+# #print(show_all_table (cursor, "sviolation_kind"))
+# #insert_row_into_table(cursor, "smark","(70,14)")
+#
+#
+#
+#
+#
+#
+#
+#
+# print("bye")
+# #cist_db.commit()
+# cist_db.close()
